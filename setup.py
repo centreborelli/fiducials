3b0b349e220583759f65be93baa2eb62c2a19612
Enric Meinhardt-Llopis <enric.meinhardt@ens-paris-saclay.fr>
2025-05-15 15:34:13 +0200

    initial commit (opencv and pyzbar)

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+import setuptools
+
+setuptools.setup(
+	name = "fiducials",
+	version = "0",
+	description = "unified interface to many fiducial marker detectors",
+	classifiers = [
+		"Operating System :: OS Independent",
+		"Topic :: Scientific/Engineering :: Image Processing",
+		"Topic :: Scientific/Engineering :: Mathematics"
+		],
+	py_modules = ["fiducials"]
+)
